c262c0f964d1a9fc1f634d6552fc7ba745ba9eb0
toeknee16 <n2zsfg9bq4@privaterelay.appleid.com>
2025-11-25 12:13:03 -0500

    Add files via upload

---
+++ monarch-config.js
@@ -0,0 +1,6 @@
+window.MonarchAbdicatorConfig = {
+  aiModel: 'gpt-5-mini',
+  regexAssistantModel: 'gpt-5-mini',
+  apiBaseUrl: 'https://gpt-worker.griffintony.workers.dev',
+  // accessCode: 'demo-1234'
+};
